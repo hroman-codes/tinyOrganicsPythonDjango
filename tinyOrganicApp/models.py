37b3b4a65fbbe44dadbf6d18bb70c4d00fb4b740
getromandev <getroman.dev@gmail.com>
2021-07-30 11:15:44 -0400

    add model for the form and init migration

--- tinyOrganicApp/models.py
+++ tinyOrganicApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
-# class CustomerForm(models.Model):
-#     firstName = models.CharField(max_length=100)
-#     lastName = models.CharField(max_length=100)
-#     email = models.EmailField(EmailField)
-#     childFirstName = models.CharField(max_length=100)
-#     childLastName = models.CharField(max_length=100)
-#     anyAlergies = models.CharField(max_length=100)
+class CustomerForm(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    childFirstName = models.CharField(max_length=100)
+    childLastName = models.CharField(max_length=100)
+    anyAlergies = models.CharField(max_length=100)
