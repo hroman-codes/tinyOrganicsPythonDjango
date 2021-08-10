a09dff2f4b5e6d9e77361aa67d6bccd5b5e5a362
getromandev <getroman.dev@gmail.com>
2021-08-10 00:39:36 -0400

    add start code functionality to fetch allergens API and spit out allergens in the view

--- tinyOrganicApp/views.py
+++ tinyOrganicApp/views.py
@@ -13,14 +13,21 @@ def form(request):
     # data = response.json()
     # print(data)
     # return render(request, 'tinyOrganicApp/test.html', {'response': response})
+
+    response = requests.get('https://60f5adf918254c00176dffc8.mockapi.io/api/v1/allergens/')
+    allergens = response.json()
     
+     
     form = CustomerForm(request.POST or None)
     if form.is_valid():
         form.save()
         form = CustomerForm()
 
     context = {
-        'form': form
+        'form': form,
+        'allergens': allergens
     }
+
+    print(context['allergens'])
 
     return render(request, 'tinyOrganicApp/form.html', context)
