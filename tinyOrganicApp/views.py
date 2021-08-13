3ae493badefd9a816e53f3d383f3e0ce2a897053
getromandev <getroman.dev@gmail.com>
2021-08-12 21:53:47 -0400

    add starter view logic for filteredRecipes

--- tinyOrganicApp/views.py
+++ tinyOrganicApp/views.py
@@ -20,3 +20,23 @@ def form(request):
     }
 
     return render(request, 'tinyOrganicApp/form.html', context)
+    # return HttpResponseRedirect('/filteredRecipes')
+
+def filteredRecipesTest(request):
+    obj = CustomerFormModel.objects.get(id=1)
+    print(obj)
+
+    # context = {
+    #     'First_Name': obj.First_Name,
+    #     'Last_Name': obj.Last_Name,
+    #     'Email': obj.Email,
+    #     'Child_First_Name': obj.Child_First_Name,
+    #     'Child_Last_Name': obj.Child_Last_Name,
+    #     'Any_Allergies': obj.Any_Allergies,
+    # }
+
+    context = {
+        'object': obj
+    }
+
+    return render(request, 'tinyOrganicApp/filteredRecipes.html', context)
