67138c197ff8f2801e0b3f0da0bbed901146e491
getromandev <getroman.dev@gmail.com>
2021-08-27 03:31:51 -0400

    clean up old code

--- tinyOrganics/settings.py
+++ tinyOrganics/settings.py
@@ -22,13 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# Add .env variables anywhere before SECRET_KEY
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
-#     dotenv.load_dotenv(dotenv_file)
-
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
