adea08862c45661c133fc8fabf254f118264c3a2
getromandev <getroman.dev@gmail.com>
2021-08-27 23:21:32 -0400

    add first set of HomePageTests

--- tinyOrganicApp/tests.py
+++ tinyOrganicApp/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
+from django.urls import reverse
 from . import views
 
 import unittest
@@ -9,6 +10,31 @@ class HomePageTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/tinyOrganicApp/')
         self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tinyOrganicApp/base.html')
+    
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/tinyOrganicApp/')
+        self.assertContains(response, '<p>Hello Tiny Organics hire your boy for this job, im trying to eat out here!</p>')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # class ApiCallsTestCase(TestCase):
